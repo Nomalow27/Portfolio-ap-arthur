ff23c8bcc7b7d9bd0b52e44250d629af4883d328
Nomalow27 <nomalow@icloud.com>
2024-12-24 19:55:22 +0100

    essaie

--- yaml/yaml.php
+++ yaml/yaml.php
@@ -8,15 +8,12 @@ require_once('vendor/autoload.php');
 use Symfony\Component\Yaml\Yaml;
 use Symfony\Component\Yaml\Exception\ParseException;
 
- 
-if (!function_exists('yaml_parse_file')) {
-    function yaml_parse_file($file){
-        try {
-            return Yaml::parseFile($file);
-        } catch (ParseException $exception) {
-            printf('Unable to parse the YAML string: <b>%s</b>', $exception->getMessage());
-        }
-    }
+function yaml_parse_file($file){
+	try {
+		return Yaml::parseFile($file);
+	} catch (ParseException $exception) {
+		printf('Unable to parse the YAML string: <b>%s</b>', $exception->getMessage());
+	}
 }
 
 function print_array($data){
